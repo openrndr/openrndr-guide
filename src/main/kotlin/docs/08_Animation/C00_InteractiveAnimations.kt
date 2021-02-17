38b3ed67cc243f9b110292ecab64824f455ba64f
Edwin Jakobs <edwin@rndr.studio>
2021-02-17 17:46:17 +0100

    Update animation chapter

--- src/main/kotlin/docs/08_Animation/C00_InteractiveAnimations.kt
+++ src/main/kotlin/docs/08_Animation/C00_InteractiveAnimations.kt
@@ -3,9 +3,11 @@
 package docs.`08_Animation`
 
 import org.openrndr.animatable.Animatable
+import org.openrndr.animatable.easing.Easing
 import org.openrndr.application
-import org.openrndr.dokgen.annotations.Code
-import org.openrndr.dokgen.annotations.Text
+import org.openrndr.color.ColorRGBa
+import org.openrndr.dokgen.annotations.*
+import org.openrndr.ffmpeg.ScreenRecorder
 
 
 fun main(args: Array<String>) {
@@ -18,180 +20,166 @@ fun main(args: Array<String>) {
 
     Anything that should be animated inherits the `Animatable` class. The Animatable class provides animation logic.
 
-    Displayed below is a very simple animation setup.
+    Displayed below is a very simple animation setup in which we animate a circle from left to right. We do this by
+    animating the `x` property of our animation object.
     """
 
-    @Code.Block
-    run {
-        application {
-            program {
-                // animation definition
-                class MyAnimatable() : Animatable() {
-                    var x: Double = 50.0
-                    var y: Double = 50.0
-                }
+    @Media.Video """media/animations-001.mp4"""
 
-                val myAnimatable = MyAnimatable()
+    @Application
+    @Code
+    application {
+        program {
+            @Exclude
+            extend(ScreenRecorder()) {
+                quitAfterMaximum = true
+                maximumDuration = 5.0
+                outputFile = "media/animations-001.mp4"
+            }
+            // -- create an animation object
+            val animation = object : Animatable() {
+                var x = 0.0
+                var y = 360.0
+            }
 
-                // sequence 1: animate x then y
-                myAnimatable.apply {
-                    ::x.animate(200.0, 3000)
-                    ::x.complete()
-                    ::y.animate(200.0, 3000)
-                    ::y.complete()
-                }
+            animation.apply {
+                ::x.animate(width.toDouble(), 5000)
+            }
 
-                // sequence 2: animate x and y simultaneously
-                myAnimatable.apply {
-                    ::x.animate(50.0, 3000)
-                    ::y.animate(50.0, 3000)
-                    ::y.complete()
-                }
+            extend {
+                animation.updateAnimation()
+                drawer.fill = ColorRGBa.PINK
+                drawer.stroke = null
+                drawer.circle(animation.x,  animation.y, 100.0)
+            }
+        }
+    }
 
-                // wait 2 seconds
-                myAnimatable.delay(2000);
 
-                // sequence 3, animate x, then animate y a bit later
-                myAnimatable.apply {
-                    ::x.animate(200.0, 3000)
-                    delay(1000)
-                    ::y.animate(200.0, 3000)
-                    ::y.complete()
-                }
+    @Text """
+    By using `.complete()` we can create sequences of property animations.
+    """
+    @Media.Video """media/animations-002.mp4"""
 
-                extend {
-                    // -- update the animation
-                    myAnimatable.updateAnimation()
-                    drawer.circle(myAnimatable.x, myAnimatable.y, 20.0)
-                }
+    @Application
+    @Code
+    application {
+        program {
+            @Exclude
+            extend(ScreenRecorder()) {
+                quitAfterMaximum = true
+                maximumDuration = 10.0
+                outputFile = "media/animations-002.mp4"
+            }
+            val animation = object : Animatable() {
+                var x = 0.0
+                var y = 0.0
+            }
+
+            animation.apply {
+                ::x.animate(width.toDouble(), 5000)
+                ::x.complete()
+                ::y.animate(height.toDouble(), 5000)
+            }
+
+            extend {
+                animation.updateAnimation()
+                drawer.fill = ColorRGBa.PINK
+                drawer.stroke = null
+                drawer.circle(animation.x,  animation.y, 100.0)
             }
         }
     }
 
     @Text """
-    For those wondering where that `::x.animate()` notation comes from, those are Kotlin's [property references](https://kotlinlang.org/docs/reflection.html#property-references).
-    
+    If we leave out that `::x.complete()` line we will see that animations for `x` and `y` run simultaneously. 
     """
+    @Media.Video """media/animations-003.mp4"""
 
-    @Text
-    """
-    ## Animation concepts
-    ### Animating
-    `animate()` queues a single animation for a single value. An animation consists of a target value and a duration and optionally a method of easing the animation.
-    """
-
-    @Text
-    """
-    ## Waiting
-
-    ### Waiting for a period of time
-
-    ```kotlin
-    fun delay(duration: Long)
-    ```
-
-    `delay()` instructs the animator to wait for the given amount of time.
-
-    ### Waiting for a moment in time
-
-    ```kotlin
-    fun waitUntil(time: Long)
-    ```
-
-    ### Waiting for an animation to finish
-
-    ```kotlin
-    fun complete(animatable: Animatable = this, callback: (Animatable) -> Unit = null)
-    // usage: with no arguments
-    myAnimatable.complete()
-
-    fun complete(variable: String)
-    fun complete(callback: (Animatable) -> Unit)
-    ```
-
-    `complete()` instructs the animator to wait until the previously queued animation has completed.
-    `complete(String variable)` instructs the animator to wait until the last queued animation for the given variable has completed.
-
-    This can be used in cases where two animations with different lengths are queued. For example:
-    """
-
+    @Application
     @Code
     application {
-        class MyAnimatable() : Animatable() {
-            var x: Double = 50.0
-            var y: Double = 50.0
-        }
-
-        val myAnimatable = MyAnimatable()
         program {
+            @Exclude
+            extend(ScreenRecorder()) {
+                quitAfterMaximum = true
+                maximumDuration = 10.0
+                outputFile = "media/animations-003.mp4"
+            }
+            val animation = object : Animatable() {
+                var x = 0.0
+                var y = 0.0
+            }
 
-            run {
-                myAnimatable.apply {
-                    ::x.animate(400.0, 2000)
-                    ::y.animate(500.0, 1200)
-                    ::x.complete()
-                }
+            animation.apply {
+                ::x.animate(width.toDouble(), 5000)
+                ::y.animate(height.toDouble(), 5000)
+            }
+
+            extend {
+                animation.updateAnimation()
+                drawer.fill = ColorRGBa.PINK
+                drawer.stroke = null
+                drawer.circle(animation.x,  animation.y, 100.0)
             }
         }
     }
 
-    @Text
-    """
-    ## Stopping animations
 
-    In interactive applications it may be needed to stop animations to accommodate user actions. 
+    @Text """
+    For those wondering where that `::x.animate()` notation comes from, those are Kotlin's [property references](https://kotlinlang.org/docs/reflection.html#property-references).
     """
 
-    @Code.Block
-    run {
-        val animation = object : Animatable() {
-            var x: Double = 0.0
-            var y: Double = 0.0
-        }
-        // set up an animation
-        animation.apply {
-            ::x.animate(1.0, 1000)
-            ::y.animate(1.0, 1000)
-        }
+    @Text """ ## Easing
+    A simple trick for making animations less stiff is to specify an easing. 
 
-        // cancel all animations
-        animation.cancel()
+    To demonstrate we take one of the previously shown animations and add easings.
+    """
+    @Media.Video """media/animations-101.mp4"""
+    @Application
+    @Code
+    application {
+        program {
+            @Exclude
+            extend(ScreenRecorder()) {
+                quitAfterMaximum = true
+                maximumDuration = 10.0
+                outputFile = "media/animations-101.mp4"
+            }
+            val animation = object : Animatable() {
+                var x = 0.0
+                var y = 0.0
+            }
+
+            animation.apply {
+                ::x.animate(width.toDouble(), 5000, Easing.CubicInOut)
+                ::y.animate(height.toDouble(), 5000, Easing.CubicInOut)
+            }
+
+            extend {
+                animation.updateAnimation()
+                drawer.fill = ColorRGBa.PINK
+                drawer.stroke = null
+                drawer.circle(animation.x,  animation.y, 100.0)
+            }
+        }
     }
 
-    @Text """In case you just want to cancel animations on a given property:"""
-
-    @Code.Block
-    run {
-        val animation = object : Animatable() {
-            var x: Double = 0.0
-            var y: Double = 0.0
-        }
-        // set up an animation
-        animation.apply {
-            ::x.animate(1.0, 1000)
-        }
-
-        // cancel only the animations on the x property
-        animation.apply {
-            ::x.cancel()
-        }
-    }
 
     @Text "## Looping animations"
     @Text """While `Animatable` doesn't provide explicit support for looping animations. They can be achieved through the following pattern:"""
 
     @Code
     application {
-        class MyAnimatable() : Animatable() {
+        val animation = object : Animatable() {
             var x: Double = 0.0
         }
 
-        val myAnimatable = MyAnimatable()
         program {
             extend {
-                myAnimatable.updateAnimation()
-                if (!myAnimatable.hasAnimations()) {
-                    myAnimatable.apply {
+                animation.updateAnimation()
+                if (!animation.hasAnimations()) {
+                    animation.apply {
                         ::x.animate(500.0, 1000)
                         ::x.complete()
                         ::x.animate(0.0, 1000)
