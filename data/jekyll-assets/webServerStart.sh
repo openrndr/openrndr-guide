229855aa31609e70490b6d6af3025cb5a078d2e2
Abe Pazos <abe@hamoid.com>
2022-04-21 20:42:41 +0200

    Do not create a new docker image each time jekyll is started.
    The previous behavior was collecting new docker images that were never removed.
    
    With this approach the server starts in seconds instead of taking a minute or so.

--- data/jekyll-assets/webServerStart.sh
+++ data/jekyll-assets/webServerStart.sh
@@ -2,8 +2,14 @@
 
 ./webServerStop.sh
 
-docker run -v $(pwd):/srv/jekyll \
-    -p 4000:4000 \
-    -i \
-    jekyll/jekyll:latest \
-    jekyll serve
+# find id of an existing container
+id=$(docker ps -a | grep jekyll | awk '{print $1;}')
+
+if [ "$id" != "" ]; then
+  # if found, run the existing container
+  docker start -i "$id"
+else
+  # otherwise create a new one and run it
+  docker run -v $(pwd):/srv/jekyll -p 4000:4000 -i jekyll/jekyll:3.8 jekyll serve
+fi
+
--- data/jekyll-assets/webServerStop.sh
+++ data/jekyll-assets/webServerStop.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# find id of running jekyll container
 id=$(docker container ls | grep jekyll | awk '{print $1;}')
 
 if [ "$id" != "" ]; then
+  # if found, stop it
   docker container stop "$id"
 fi
 
