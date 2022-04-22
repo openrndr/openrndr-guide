53e71cda90015ffa083c73c23d30d906df1ed5d6
Abe Pazos <abe@hamoid.com>
2022-04-22 11:15:44 +0200

    Simpler docker commands (by container name)

--- data/jekyll-assets/webServerStart.sh
+++ data/jekyll-assets/webServerStart.sh
@@ -2,14 +2,8 @@
 
 ./webServerStop.sh
 
-# find id of an existing container
-id=$(docker ps -a | grep jekyll | awk '{print $1;}')
+name=www-openrndr-guide
 
-if [ "$id" != "" ]; then
-  # if found, run the existing container
-  docker start -i "$id"
-else
-  # otherwise create a new one and run it
-  docker run -v $(pwd):/srv/jekyll -p 4000:4000 -i jekyll/jekyll:3.8 jekyll serve
-fi
+docker start -ai $name || \
+  docker run --name=$name -v $(pwd):/srv/jekyll -p 4000:4000 -i jekyll/jekyll:latest jekyll serve
 
--- data/jekyll-assets/webServerStop.sh
+++ data/jekyll-assets/webServerStop.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# find id of running jekyll container
-id=$(docker container ls | grep jekyll | awk '{print $1;}')
+name=www-openrndr-guide
 
-if [ "$id" != "" ]; then
-  # if found, stop it
-  docker container stop "$id"
-fi
-
+docker container stop $name
 
