92510570f832142ee3c14dcea363687d4f99da6d
Abe Pazos <abe@hamoid.com>
2022-08-23 10:56:27 +0200

    Mention toggling video recording, video formats

--- src/main/kotlin/docs/09_Videos/C110_Writing_to_video_files.kt
+++ src/main/kotlin/docs/09_Videos/C110_Writing_to_video_files.kt
@@ -6,6 +6,7 @@
 
 package docs.`09_Videos`
 
+import org.openrndr.KEY_ESCAPE
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.dokgen.annotations.*
@@ -18,11 +19,10 @@ fun main() {
 
     @Text
     """
-    # Writing to video files #
+    # Writing to video files
 
     ## Prerequisites
 
-    Add the `openrndr-ffmpeg` library to your project.
     Make sure ffmpeg is installed on your system.
 
     ## Writing to video using render targets
@@ -62,15 +62,15 @@ fun main() {
 
     @Text
     """
-    ## The ScreenRecorder extension
+    ## The `ScreenRecorder` extension
 
     A much simpler way of writing your program's output to video is
     offered by the `ScreenRecorder` extension. The extension creates video 
     files named after your Program class name plus the date. 
     For example: `MyProgram-2018-04-11-11.31.03.mp4`. The video files 
-    are located in the current working directory.
+    are located in the `video/` directory in your project.
 
-    To setup the screen recorder you do the following:
+    To setup the screen recorder do the following:
     """
 
     @Code
@@ -78,8 +78,55 @@ fun main() {
         program {
             extend(ScreenRecorder())
             extend {
-                // -- whatever you do here ends up in the video
+                // -- whatever you draw here ends up in the video
             }
         }
     }
+
+    @Text
+    """
+    ### Toggle the `ScreenRecorder` on and off
+    
+    By default the screen recorder extension adds video frames as long as the
+    program runs. If you keep running the program again and again you will end
+    up with a folder full of video files.
+    
+    We can make it more flexible by letting the user start and pause the
+    recorder, for instance by pressing the `v` key on the keyboard.
+    """
+
+    @Code
+    application {
+        program {
+            // keep a reference to the recorder so we can start it and stop it.
+            val recorder = ScreenRecorder().apply {
+                outputToVideo = false
+            }
+            extend(recorder)
+            extend {
+                // -- draw things here
+            }
+            keyboard.keyDown.listen {
+                when {
+                    it.key == KEY_ESCAPE -> program.application.exit()
+                    it.name == "v" -> {
+                        recorder.outputToVideo = !recorder.outputToVideo
+                        println(if (recorder.outputToVideo) "Recording" else "Paused")
+                    }
+                }
+            }
+        }
+    }
+
+    @Text
+    """
+    ## Video formats
+    
+    By default videos are recorded in h264 format with a `.mp4` file extension,
+    but animated `gif` or `webp`, `png` or `tif` sequences, `prores` and `h265` 
+    are also available by enabling the 
+    [orx-video-profiles](https://github.com/openrndr/orx/tree/master/orx-jvm/orx-video-profiles)
+    extension.
+        
+    """
 }
