cb40b4c5349439251993532c4886f2d797fdd8ee
Abe Pazos <abe@hamoid.com>
2023-04-29 15:30:56 +0200

    Add file-scopes task

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,3 +100,28 @@ val publishExamples by tasks.registering {
     }
     finalizedBy(gitPublishPush)
 }
+
+task("add IDE file scopes") {
+    group = " ★ OPENRNDR"
+    val scopesFolder = File("${project.projectDir}/.idea/scopes")
+    scopesFolder.mkdirs()
+
+    val files = listOf(
+        "Code" to "file:*.kt||file:*.frag||file:*.vert||file:*.glsl",
+        "Text" to "file:*.txt||file:*.md||file:*.xml||file:*.json",
+        "Gradle" to "file[*buildSrc*]:*/||file:*gradle.*||file:*.gradle||file:*/gradle-wrapper.properties||file:*.toml",
+        "Images" to "file:*.png||file:*.jpg||file:*.dds||file:*.exr"
+    )
+    files.forEach { (name, pattern) ->
+        val file = File(scopesFolder, "__$name.xml")
+        if (!file.exists()) {
+            file.writeText(
+                """
+                    <component name="DependencyValidationManager">
+                      <scope name=" ★ $name" pattern="$pattern" />
+                    </component>
+                    """.trimIndent()
+            )
+        }
+    }
+}
