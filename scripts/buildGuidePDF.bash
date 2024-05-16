ce611b0ffae4e9b0dbf45c51be44cd41f1dd71c3
Abe Pazos <abe@hamoid.com>
2024-05-16 16:07:03 +0200

    Make buildGuidePDF.bash easier to reuse in other domains

--- scripts/buildGuidePDF.bash
+++ scripts/buildGuidePDF.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+filename="openrndr-guide"
+domain="guide.openrndr.org"
+path="" # /some/folder/ if the guide is not located at /
+
 mkdir -p /tmp/manual
 cd /tmp/manual || exit
 
@@ -9,7 +13,7 @@ cd /tmp/manual || exit
 # sed(2) deletes from each line the " character and everything that follows, leaving the clean URL
 # tail deletes the first line, which contains a lonely <nav> tag
 
-urlstr=$(curl -s "https://guide.openrndr.org" | grep -o -E '<nav .*</nav>' | sed "s/href=\"\//href=\"\nhttps:\/\/guide.openrndr.org\//g" | sed "s/\".*//g" | tail +2)
+urlstr=$(curl -s "https://$domain$path" | grep -o -E '<nav .*</nav>' | sed "s/href=\"\//href=\"\nhttps:\/\/$domain\//g" | sed "s/\".*//g" | tail +2)
 
 # convert a long string into an array
 urls=($urlstr)
@@ -22,6 +26,7 @@ echo "Found $length URLs"
 # one by one create NNNN.pdf files from each URL
 for (( i=0; i<${length}; i++ ));
 do
+  echo "# Page $i of $length"
   padded=$(printf "%04d" $i)
   wkhtmltopdf ${urls[$i]} $padded.pdf
 done
@@ -29,4 +34,5 @@ done
 date=$(date +"%F")
 
 # finally join all the PDF files into one
-pdfunite *.pdf /tmp/openrndr-guide-$date.pdf
+pdfunite *.pdf /tmp/$filename-$date.pdf
+
