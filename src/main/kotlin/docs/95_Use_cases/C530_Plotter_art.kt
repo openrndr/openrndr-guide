60130b272d6dd9ebc65484363d2ff4ce95dfbefe
Abe Pazos <abe@hamoid.com>
2024-10-21 09:05:22 +0200

    Extend Plotter_art document

--- src/main/kotlin/docs/95_Use_cases/C530_Plotter_art.kt
+++ src/main/kotlin/docs/95_Use_cases/C530_Plotter_art.kt
@@ -7,8 +7,11 @@
 package docs.`95_Use_cases`
 
 import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.dialogs.saveFileDialog
 import org.openrndr.dokgen.annotations.*
 import org.openrndr.drawComposition
+import org.openrndr.shape.draw
 import org.openrndr.svg.saveToFile
 import java.io.File
 
@@ -65,7 +68,76 @@ fun main() {
     The API in the composition drawer is almost identical
     to the one of the standard drawer: we can use methods like
     `segment`, `contour`, `shape`, `circle`, `rectangle`, etc.
-        
+    
+    Note: if you are using OPENRNDR / ORX version 0.4.5 you need to
+    enable `orx-composition` and `orx-svg` in the `build.gradle.kts` file.
+
+    ## Interaction
+    
+    Lets take our simple program a step further and make it interactive.
+    We will generate a new design every time the mouse is clicked and
+    save the design when we press the `s` key on the keyboard.
+    """
+
+    @Code
+    application {
+        program {
+            // Create an empty composition
+            val design = drawComposition { }
+
+            // A function to draw concentric circles into the composition.
+            fun generateDesign() {
+                design.clear()
+                val separation = 4.0
+                val steps = (drawer.bounds.center.distanceTo(mouse.position) / separation).toInt()
+                design.draw {
+                    repeat(steps) {
+                        circle(drawer.bounds.center, (it + 1) * separation)
+                    }
+                }
+            }
+
+            // Draw the composition onto the window
+            extend {
+                drawer.clear(ColorRGBa.WHITE)
+                drawer.fill = null
+                drawer.composition(design)
+            }
+
+            // Generate a new design every time we click the mouse
+            mouse.buttonDown.listen {
+                generateDesign()
+            }
+
+            // Show a save dialog when pressing the `s` key, then save the design
+            // with the chosen file name into the selected folder.
+            keyboard.keyDown.listen {
+                if (it.name == "s") {
+                    saveFileDialog(supportedExtensions = listOf("SVG" to listOf("svg"))) { file ->
+                        design.saveToFile(file)
+                    }
+                }
+            }
+        }
+    }
+
+    @Text
+    """
+    With the last program we could create minimal designs by clicking
+    the mouse button on different locations on the program window and
+    then save the design when pressing the `s` key.
+    
+    If we open the resulting design in Inkscape we will notice that the
+    document size is 640 pixels wide and 480 pixels height, matching the
+    default OPENRNDR window size. In Inkscape we could choose a different
+    document size (A4 for instance) then re-center and scale the design to
+    match our preferences.
+    
+    Lets find out how to specify the SVG document dimensions, 
+    and how to make what we see in the window match what gets exported
+    as SVG. (Coming soon...)
+    
+           
     Find more tips on
     [using OPENRNDR with pen plotters](https://openrndr.discourse.group/t/openrndr-plotting-tricks-axidraw-etc/208)
     in the forum.
