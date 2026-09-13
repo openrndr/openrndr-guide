d79dfd488d89d43773d36c7d19cce680372b5ad7
Abe Pazos <abe@hamoid.com>
2026-09-13 17:27:14 +0200

    Try 0.5.0-SNAPSHOT in next-version branch

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,14 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
+        maven("https://central.sonatype.com/repository/maven-snapshots/") {
+            name = "Central Portal Snapshots"
+
+            content {
+                includeGroup("org.openrndr")
+                includeGroup("org.openrndr.extra")
+            }
+        }
     }
 }
 
