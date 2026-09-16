529feef6fee3c4d488a4edbe679657f41f442168
Abe Pazos <abe@hamoid.com>
2026-09-16 21:19:16 +0200

    [build] Remove snapshot dependency
    0.5.0 was published today

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,14 +4,6 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
-        maven("https://central.sonatype.com/repository/maven-snapshots/") {
-            name = "Central Portal Snapshots"
-
-            content {
-                includeGroup("org.openrndr")
-                includeGroup("org.openrndr.extra")
-            }
-        }
     }
 }
 
